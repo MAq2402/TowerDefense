1c813feb7d8db033a045baec64926f7d8c04a0a2
martyna8821 <martyna.drabinska@gmail.com>
2019-04-02 17:15:56 +0200

    moving on scene with mouse

--- TowerDefense/Assets/Scripts/CameraController.cs
+++ TowerDefense/Assets/Scripts/CameraController.cs
@@ -8,6 +8,10 @@ public class CameraController : MonoBehaviour
     public float yMin = 10f;
     public float yMax = 80f;
 
+    public float mDelta = 10f;
+    public float mSpeed = 20f;
+
+    public float rSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +22,37 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        zoom();
+        translate();
+                 
+    }
 
+    void zoom()
+    {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         Vector3 position = transform.position;
         position.y -= scroll * scrollSpeed * 1000 * Time.deltaTime;
         position.y = Mathf.Clamp(position.y, yMin, yMax);
-        transform.position = position;        
+        transform.position = position;
+    }
+
+    void translate()
+    {
+        if (Input.mousePosition.x <= mDelta || Input.GetKey("left"))
+        {
+            transform.Translate(Vector3.left * mSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.mousePosition.x >= Screen.width - mDelta || Input.GetKey("right"))
+        {
+            transform.Translate(Vector3.right * mSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.mousePosition.y <= mDelta || Input.GetKey("down"))
+        {
+            transform.Translate(Vector3.back * mSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.mousePosition.y >= Screen.height - mDelta || Input.GetKey("up"))
+        {
+            transform.Translate(Vector3.forward * mSpeed * Time.deltaTime, Space.World);
+        }
     }
 }
