e18f743018d62ecc81fd7a2d6cd43f5c16fa37e3
Michal Miciak <maq0297@gmail.com>
2019-05-10 22:06:42 +0200

    ally spawning works well, def turrets improvements

--- TowerDefense/Assets/Scripts/Turrets/DefensiveTurret.cs
+++ TowerDefense/Assets/Scripts/Turrets/DefensiveTurret.cs
@@ -1,49 +1,102 @@
 ﻿using Assets.Scripts.Utility;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DefensiveTurret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Transform spawnPoint;
-    public Transform ally;
-    private bool hasBeenClicked = false;
+    private const string GROUND = "Ground";
+    public GameObject ally;
+    private bool hasBeenAlreadyClicked = false;
     private float range = 5f;
+    private bool allyHasBeenSpawned = false;
+    private int allyHealth = 3;
 
     public virtual int Cost { get; set; } = 100;
     void Start()
     {
-        SpawnAlly();
-    }
 
-    private void SpawnAlly()
-    {
-        //Instantiate(ally, spawnPoint.position, spawnPoint.rotation);
-        //For now do nothing :: WILL BE DONE SOON
     }
 
     private void OnMouseDown()
     {
-        hasBeenClicked = !hasBeenClicked;
-        if(hasBeenClicked)
-        {
-            Drawer.DrawCircleOnSurface(gameObject.GetComponent<LineRenderer>(), this.range, 0.25f, Color.green);
-        }
-        else
-        {
-            Drawer.DrawEmpty(gameObject.GetComponent<LineRenderer>());
-        }
+
     }
     private void OnMouseUp()
     {
-        
+
     }
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform == transform)
+                {
+                    UseDrawer();
+                }
+                else
+                {
+                    CallAlly(ray, hit);
+                    HideRange();
+                }
+            }
+        }
 
     }
+    private void UseDrawer()
+    {
+        if (hasBeenAlreadyClicked)
+        {
+            HideRange();
+        }
+        else
+        {
+            ShowRange();
+        }
+    }
+
+    private void ShowRange()
+    {
+        Drawer.DrawCircleOnSurface(gameObject.GetComponent<LineRenderer>(), this.range, 0.25f, Color.green);
+    }
+
+    private void HideRange()
+    {
+        Drawer.DrawEmpty(gameObject.GetComponent<LineRenderer>());
+    }
+
+    private void CallAlly(Ray ray, RaycastHit hit)
+    {
+        if (allyHasBeenSpawned)
+        {
+            MoveAlly();
+        }
+        else
+        {
+            SpawnAlly(hit);
+        }
+    }
+    private void MoveAlly()
+    {
+        Debug.Log("Move ally");
+    }
+
+    private void SpawnAlly(RaycastHit hit)
+    {
+        var distanceBetween = Vector3.Distance(hit.point, transform.position);
+        if (distanceBetween < range && hit.transform.gameObject.name.Contains(GROUND))
+        {
+            Instantiate(ally, new Vector3(hit.point.x, hit.point.y, hit.point.z), hit.transform.rotation);
+            allyHasBeenSpawned = true;
+        }
+    }
+
     void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(1))
