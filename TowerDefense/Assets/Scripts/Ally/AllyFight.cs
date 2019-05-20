07f0540a6e95b9c0fd32be900fd3a665e09de432
Michal Miciak <maq0297@gmail.com>
2019-05-20 21:50:57 +0200

    ally fighting implemented

--- TowerDefense/Assets/Scripts/Ally/AllyFight.cs
+++ TowerDefense/Assets/Scripts/Ally/AllyFight.cs
@@ -9,43 +9,74 @@ namespace Assets.Scripts.Ally
 {
     public class AllyFight : MonoBehaviour
     {
-        private double allyHealth = 200;
+        private double allyHealth = 500;
         private GameObject enemy;
         private EnemyHealth enemyHealth;
         private EnemyMovement enemyMovement;
         private Collider allyCollider;
+        private Animator animator;
         private void OnCollisionEnter(Collision collision)
         {
-            if (collision.collider.gameObject.GetComponents<EnemyMovement>().Any())
+            if (collision.collider.gameObject.GetComponents<EnemyMovement>().Any() && enemy == null)
             {
                 enemy = collision.collider.gameObject;
+
+                StartAnimating();
 
                 enemyMovement = enemy.gameObject.GetComponent<EnemyMovement>();
                 enemyMovement.Fight(transform);
 
                 enemyHealth = enemy.gameObject.GetComponent<EnemyHealth>();
-                
+
                 allyCollider = GetComponent<Collider>();
                 allyCollider.enabled = false;
             }
 
         }
 
+        private void StartAnimating()
+        {
+            animator = gameObject.GetComponent<Animator>();
+            animator.enabled = true;
+            animator.SetTrigger("Aiming");
+            animator.SetTrigger("Fire");
+        }
+
         private void Update()
         {
             if(enemy != null)
             {
-                allyHealth -= 25;
-                enemyHealth.DecreaseHealth();
+                FaceTowardsEnemy();
+                allyHealth -= 10;
+                enemyHealth.DecreaseHealth(0.1f);
                 allyCollider.enabled = true;
-                
+            }
+            else if(animator !=null)
+            {
+                if(animator.GetCurrentAnimatorStateInfo(0).IsName("Aiming"))
+                {
+                    animator.ResetTrigger("Aiming");
+                }
+
+                if(animator.GetCurrentAnimatorStateInfo(0).IsName("Fire"))
+                {
+                    animator.ResetTrigger("Fire");
+                }
             }
 
-            if(allyHealth <= 0)
+            if (allyHealth <= 0)
             {
+                //Will work on that
                 enemyMovement.StopFight();
-                Destroy(gameObject);
+                gameObject.SetActive(false);
+                //Destroy(gameObject);
             }
+        }
+
+        private void FaceTowardsEnemy()
+        {
+            var targetRotation = Quaternion.LookRotation(enemy.transform.position - transform.position);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 1);
         }
     }
 }
