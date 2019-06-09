b2a31a751c4f785923012f42ace8f7a27e5c22a9
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2019-06-10 00:12:28 +0200

    missile launcher work started

---
+++ TowerDefense/Assets/Scripts/Bullets/LauncherProjectile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: Bartłomiej Krasoń
+ * LauncherProjectile class represents specific projectile for: 
+ *  MissileLauncherTurret
+ */
+public class LauncherProjectile : Projectile
+{
+    public float speed = 70f;
+    private Vector3 startPosition;
+    private float highRatio = 0.5f;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    /* Author: Bartłomiej Krasoń */
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        var currentPosition = transform.position;
+
+        var distanceFromTarget = Vector3.Distance(targetCenter, currentPosition);
+        var distanceFromStartPosition = Vector3.Distance(currentPosition, startPosition);
+
+        var direction = this.targetCenter - transform.position;
+        if (distanceFromStartPosition < distanceFromTarget)
+        {// before half-a-way
+            direction.y += highRatio;
+        }
+        else
+        {//after half-a-way
+            direction.y -= highRatio;
+        }
+        var distanceThisFrame = speed * Time.deltaTime;
+
+        if(direction.magnitude <= distanceThisFrame)
+        {
+            Hit();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+}
---
+++ TowerDefense/Assets/Scripts/Turrets/MissileLauncherTurret.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: Bartłomiej Krasoń
+ *  MissileLauncherTurret class represents specific data/actions for Missile Launcher 
+ */
+public class MissileLauncherTurret : PrimaryTurret
+{
+    [Header("MissileLauncherTurret specifications")]
+    public Transform verticalRotatingPart;
+
+    public override int Cost { get; set; } = 300;
+
+    /* Author: Bartłomiej Krasoń */
+    void Start()
+    {
+        base.OnStart();
+    }
+
+    /* Author: Bartłomiej Krasoń */
+    void Update()
+    {
+        base.OnUpdate();
+    }
+
+    /* Author: Bartłomiej Krasoń */
+    protected override void UpdateView()
+    {
+        if (this.target)
+        {
+            //rotate horizontal
+            Vector3 directionVector = this.target.transform.position - this.transform.position;
+            Quaternion fromQuaterion = this.rotatingPart.rotation;
+            Quaternion toQuaternion = Quaternion.LookRotation(directionVector);
+            var rotationAnglesHorizontal = Quaternion.RotateTowards(fromQuaterion, toQuaternion, this.rotateSpeed).eulerAngles;
+            this.rotatingPart.rotation = Quaternion.Euler(0.0f, rotationAnglesHorizontal.y, 0.0f);
+
+            //rotate vertical
+            Vector3 directionVectorVertical = this.target.transform.position - this.verticalRotatingPart.transform.position;
+            Quaternion fromQuaternionVerical = this.verticalRotatingPart.rotation;
+            Quaternion toQuaternionVertical = Quaternion.LookRotation(directionVectorVertical);
+            var rotationAnglesVertical = Quaternion.RotateTowards(fromQuaternionVerical, toQuaternionVertical, this.rotateSpeed*10.0f).eulerAngles;
+            this.verticalRotatingPart.rotation = Quaternion.Euler(90.0f + rotationAnglesVertical.x, rotationAnglesHorizontal.y, 0.0f);            
+        }
+    }
+}
